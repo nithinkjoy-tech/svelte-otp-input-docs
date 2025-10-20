39d0e4321e6b34568dd859eec078b1be8199ea9e
Nithin K Joy <nithinjoy121212@gmail.com>
2025-10-20 11:42:44 +0530

    302 Redirection URL update

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,19 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 
-export const prerender = false;
-
 export function load({ url }) {
-	const pathname = url.pathname;
-
-	// Production path
-	if (pathname === '/docs/svelte-otp-input/' || pathname === '/docs/svelte-otp-input') {
-		throw redirect(302, '/docs/svelte-otp-input/basic-usage');
-	}
-
-	// Development path
-	if (pathname === '/') {
+	if (url.pathname === '/' || url.pathname === '') {
 		throw redirect(302, '/basic-usage');
 	}
-
 	return {};
 }
